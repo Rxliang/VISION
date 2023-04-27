bf12f404a094184b596f91515cbed96d86e207eb
Xiangyu Zhang <xzhan233@b17.clsp.jhu.edu>
2023-04-26 23:38:12 -0400

    new

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l gpu=1,ram_free=30G,mem_free=30G,hostname=c*
+#$ -l gpu=1,ram_free=30G,mem_free=30G,hostname=b1[123456789]|c0*|c1[123456789]
 #$ -e /home/xzhan233/MIND/logs/result.log
 #$ -o /home/xzhan233/MIND/logs/result.txt
 #$ -q g.q
