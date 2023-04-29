00c21e6d572fafa05988537de193f6083b6d2de1
Tony <you@example.com>
2023-04-28 20:57:54 -0400

    save_folder

--- run.sh
+++ run.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-#$ -l gpu=1,ram_free=40G,mem_free=40G,hostname=b18
-#$ -e /home/xzhan233/MIND/logs/result.log
-#$ -o /home/xzhan233/MIND/logs/result.txt
-#$ -q g.q
-
 yaml_location=./config.yaml
-
 python3 ./main.py --config ${yaml_location}
