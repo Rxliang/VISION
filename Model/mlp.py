d4d3b1092ca0aeca9404e7183b66f7851d9cc5df
Simon_Leung <lrx9809@outlook.com>
2023-05-09 09:47:39 -0400

    siren

--- Model/mlp.py
+++ Model/mlp.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from mlp_mixer_pytorch import MLPMixer
 torch.manual_seed(3407)
+from siren_pytorch import SirenNet
+
 
 device = 'cuda'
 
@@ -16,12 +18,21 @@ class MLP_model:
 
     def init_MLP_Mixer(self):
 
-        model = MLPMixer(
-            image_size = self.image_size,
-            channels = self.channels,
-            patch_size = self.patch_size,
-            dim = self.dim,
-            depth = self.depth,
-            num_classes = self.num_classes
-        )
-        return model
+        # model = MLPMixer(
+        #     image_size = self.image_size,
+        #     channels = self.channels,
+        #     patch_size = self.patch_size,
+        #     dim = self.dim,
+        #     depth = self.depth,
+        #     num_classes = self.num_classes
+        # )
+        # return model
+        net = SirenNet(
+            dim_in = 768,                        # input dimension, ex. 2d coor
+            dim_hidden = 320,                  # hidden dimension
+            dim_out = 19004,                       # output dimension, ex. rgb value
+            num_layers = 2,                    # number of layers
+            final_activation = nn.Identity(),   # activation of final layer (nn.Identity() for direct output)
+            w0_initial = 30.                   # different signals may require different omega_0 in the first layer - this is a hyperparameter
+        ).to(device)
+        return net
